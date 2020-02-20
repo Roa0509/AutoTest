4005d94d6c4ccb5b0d2142aa85a916526695df4f
Roa0509 <engineerxy@126.com>
2020-02-20 15:45:54 +0800

    第二天学习代码提交

---
+++ APITest3/src/main/java/com/roa/httpclient/myHttpClient.java
@@ -0,0 +1,39 @@
+package com.roa.httpclient;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class myHttpClient {
+
+    private String url;
+    private ResourceBundle bundle;
+
+    @BeforeTest
+    public void beforeTest(){
+        bundle = ResourceBundle.getBundle("application", Locale.CHINA);
+        url = bundle.getString("test.url");
+    }
+    @Test
+    public void login() throws IOException {
+        String result;
+        String uri;
+        uri = this.bundle.getString("test.uri");
+        System.out.println(uri);
+        String test_rul = this.url+uri;
+        HttpGet get = new HttpGet(test_rul);
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+    }
+
+}
